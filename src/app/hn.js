5a0e09f62208349dfe6878f63c9d9098ac497305
devmetal <devmetal91@gmail.com>
2016-01-04 16:36:39 +0100

    remove item component

--- src/app/hn.js
+++ src/app/hn.js
@@ -26,54 +26,6 @@ class Preloader {
 }
 
 @Component({
-  selector: 'item',
-  template: `
-  <li>
-    <div class='collapsible-header'>
-      {{item.title}}
-      <span class='badge'>{{item.score}}</span>
-    </div>
-    <div class='collapsible-body'>
-      <p>
-        <a class="waves-effect waves-light btn" [href]='item.url' target='_blank'><i class="material-icons left">launch</i>Read more</a>
-        <a class="waves-effect waves-light btn"><i class="material-icons left">favorite</i>Favorite</a>
-        <a class="waves-effect waves-light btn"><i class="material-icons left">share</i>Facebook</a>
-        <a class="waves-effect waves-light btn"><i class="material-icons left">share</i>Twitter</a>
-      </p>
-      <div *ngFor='#kid of kids'>
-        <p>{{kid.text}}</p>
-      </div>
-    </div>
-  </li>
-  `,
-  directives:[NgFor]
-})
-class Item {
-  @Input()
-  item;
-
-  constructor(db: HnDb) {
-    this.db = db;
-    this.kids = [];
-    this.pending = false;
-  }
-
-  loadKids() {
-    this.fetchKids();
-    return true;
-  }
-
-  async fetchKids() {
-    if (!this.pending && !this.kids.length) {
-      this.pending = true;
-      this.kids = await this.db.kids(this.item);
-      console.log(this.kids);
-      this.pending = false;
-    }
-  }
-}
-
-@Component({
   selector:'items',
   template:`
   <ul class='collapsible' data-collapsible='accordion'>
