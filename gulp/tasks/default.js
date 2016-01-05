3b1c0124e84bbab09f0f71155bd13f9fc864d736
devmetal <devmetal91@gmail.com>
2016-01-05 17:34:25 +0100

    add qr code feature to gulp

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,5 +1,24 @@
 'use strict';
 
 let gulp = require('gulp');
+let os          = require('os');
+var qrcode      = require('qrcode-terminal');
 
-gulp.task('default', ['serve']);
+let getIp = () => {
+  let ifaces = os.networkInterfaces();
+  return Object.keys(ifaces)
+    .map(ifname => ifaces[ifname])
+    .reduce((iface) => {
+      return iface.filter(face => face.family === 'IPv4' && !face.internal);
+    });
+}
+
+gulp.task('default', ['serve'], function() {
+  let ip = getIp();
+  if (ip) {
+    if (Array.isArray(ip) && ip.length) {
+      ip = ip[0];
+    }
+    qrcode.generate(`http://${ip.address}:3000`);
+  }
+});
